0a9954a62721d47c3489d50f2a4168be685ef865
navarretemarce <m.a.navarrete@me.com>
2021-07-14 18:54:16 -0300

    python > python3

--- alphafold.sh
+++ alphafold.sh
@@ -11,7 +11,7 @@ for replica in 0 1 2 3; do
 	if [ $replica -eq 0 ]; then mode="D B T"; else mode="D B"; fi
 	for m in $mode; do
 		echo "Launching model: ${m} ${replica}"
-		python alphafold.py -i $TARGET_FILE -o $OUTPUT_DIR -m $MODEL_DIR -r $replica -t $m | cat &
+		python3 alphafold.py -i $TARGET_FILE -o $OUTPUT_DIR -m $MODEL_DIR -r $replica -t $m | cat &
 	done
 done
 
@@ -19,4 +19,4 @@ echo -e "All models running, waiting for them to complete\n"
 wait
 
 echo "Ensembling all replica outputs & Pasting contact maps"
-python alphafold.py -i $TARGET_FILE -o $OUTPUT_DIR -e
+python3 alphafold.py -i $TARGET_FILE -o $OUTPUT_DIR -e
